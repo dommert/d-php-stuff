d977dc0deef9f45366a4eb73488385f37a58e37b
Dommert <DommertEnterprises@Gmail.com>
2014-03-29 21:18:42 -0700

    SourceTree test

--- themes/default/main.php
+++ themes/default/main.php
@@ -12,11 +12,11 @@ REQUIRE_ONCE($config);
 
 // Check Login
 
-
 ?> 
 
 <!doctype html>
 <html class="no-js" lang="en">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -24,49 +24,48 @@ REQUIRE_ONCE($config);
   <link rel="stylesheet" href="<?PHP ECHO $css.'foundation.css'; ?>" />
   <script src="<?PHP ECHO $modernizr; ?>"></script>
 </head>
+
 <body>
+  
   <header>     
     <div class="row">
       <div class="large-12 columns">
-        
         <h1>Welcome to Foundation</h1>
-        <nav class="top-bar" data-topbar> 
-          <ul class="title-area"> 
-            <li class="name"> <h1><a href="#">Domain</a></h1> </li> 
-            <li class="toggle-topbar menu-icon"><a href="#">HOME</a></li>
-          </ul> 
+          
+          <nav class="top-bar" data-topbar> 
+            
+            <ul class="title-area"> 
+              <li class="name"> <h1><a href="#">Domain</a></h1> </li> 
+              <li class="toggle-topbar menu-icon"><a href="#">HOME</a></li>
+            </ul> 
          
-          <section class="top-bar-section"> 
-      <!-- Right Nav Section --> 
-            <ul class="right"> 
-              <li class="active"> <a href="#">Right Button</a></li> 
-                <li class="has-dropdown"> <a href="#">Admin</a> 
-                <ul class="dropdown"> 
-                  <li><a href="#">Users</a></li>
-                  <li><a href="#">Objects</a></li>
-                  <li><a href="#">Files</a></li>
-                  <li><a href="#">Links</a></li> 
-                </ul> 
-            </ul> 
-      <!-- Left Nav Section --> 
-            <ul class="left"> 
-              <li><a href="#">HOME</a></li> 
-              <li><a href="#">Shared</a></li> 
-              <li><a href="#">Settings</a></li> 
-
-
-            </ul> 
-          </section> 
-        </nav>
+            <section class="top-bar-section"> 
+        <!-- Right Nav Section --> 
+              <ul class="right"> 
+                <li class="active"> <a href="#">Right Button</a></li> 
+                  <li class="has-dropdown"> <a href="#">Admin</a> 
+                  <ul class="dropdown"> 
+                    <li><a href="#">Users</a></li>
+                    <li><a href="#">Objects</a></li>
+                    <li><a href="#">Files</a></li>
+                    <li><a href="#">Links</a></li> 
+                  </ul> 
+              </ul> 
+        <!-- Left Nav Section --> 
+              <ul class="left"> 
+                <li><a href="#">HOME</a></li> 
+                <li><a href="#">Shared</a></li> 
+                <li><a href="#">Settings</a></li> 
+              </ul> 
+            </section> 
+          </nav>
       </div>
     </div>
   </header>
+
   <div id="middle"> 
-
-    <div class="row">
-
-
-  <div class="large-12 columns">
+   <div class="row">
+    <div class="large-12 columns">
     <h1>middle</h1>
   </div>
     </div>
