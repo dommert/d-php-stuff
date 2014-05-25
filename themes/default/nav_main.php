d2107a59cf35de8e3e33e1cea1705aea81f6c411
Dommert <DommertEnterprises@Gmail.com>
2014-05-25 01:17:27 -0700

    nav
    playing with nav

--- themes/default/nav_main.php
+++ themes/default/nav_main.php
@@ -1,28 +1,68 @@
-<nav class="top-bar" data-topbar> 
-          <ul class="title-area"> 
-            <li class="name"> <h1><a href="#">Domain.com</a></h1> </li> 
-            <li class="toggle-topbar menu-icon"><a href="#">HOME</a></li>
-          </ul> 
-         
-          <section class="top-bar-section"> 
-      <!-- Right Nav Section --> 
-            <ul class="right"> 
-              <li class="active"> <a href="#">Right Button</a></li> 
-                <li class="has-dropdown"> <a href="#">Admin</a> 
-                <ul class="dropdown"> 
-                  <li><a href="#">Users</a></li>
-                  <li><a href="#">Objects</a></li>
-                  <li><a href="#">Files</a></li>
-                  <li><a href="#">Links</a></li> 
-                </ul> 
-            </ul> 
-      <!-- Left Nav Section --> 
-            <ul class="left"> 
-              <li><a href="#">HOME</a></li> 
-              <li><a href="#">Shared</a></li> 
-              <li><a href="#">Settings</a></li> 
+  <div id="nav_main" class="row">
+  <div class="contain-to-grid sticky">
+  <nav class="top-bar large-12 medium-12 columns" data-topbar data-options="sticky_on: medium">
+    <ul class="title-area">
+     <!-- Title Area -->
+      <li class="name">
+        <h1>
+          <a href="#">
+            Domain.com
+          </a>
+        </h1>
+      </li>
 
+      <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+    </ul>
 
-            </ul> 
-          </section> 
-        </nav>
+<section class="top-bar-section">
+      <!-- Right Nav Section -->
+      <ul class="left">
+           <li class="has-form"> <a href="#" class="button">Home</a> </li>
+
+      </ul>
+      <ul class="right">
+
+        <li class="divider"></li>
+        <li class="has-dropdown not-click">
+          <a href="#">Menu</a>
+          <ul class="dropdown"><li class="title back js-generated"><h5><a href="javascript:void(0)">Back</a></h5></li>
+            <li><label>Section Name</label></li>
+            <li class="has-dropdown not-click">
+              <a href="#" class="">Has Dropdown, Level 1</a>
+              <ul class="dropdown"><li class="title back js-generated"><h5><a href="javascript:void(0)">Back</a></h5></li>
+                <li><a href="#">Dropdown Options</a></li>
+                <li><a href="#">Dropdown Options</a></li>
+                <li><a href="#">Level 2</a></li>
+                <li><a href="#">Subdropdown Option</a></li>
+                <li><a href="#">Subdropdown Option</a></li>
+                <li><a href="#">Subdropdown Option</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li class="divider"></li>
+            <li><label>Section Name</label></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li class="divider"></li>
+            <li><a href="#">See all →</a></li>
+          </ul>
+        </li>
+
+        <li class="divider"></li>
+        
+        <li class="has-dropdown not-click">
+          <a href="#">Admin</a>
+          <ul class="dropdown"><li class="title back js-generated"><h5><a href="javascript:void(0)">Back</a></h5></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li><a href="#">Dropdown Option</a></li>
+            <li class="divider"></li>
+            <li><a href="#">See all →</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section></nav>
+    </div>
+    </div>
