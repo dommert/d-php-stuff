6f6946e659ac47e98fe5a67497d652cc47dd2709
Dommert <DommertEnterprises@Gmail.com>
2014-05-25 05:12:42 -0700

    ---

--- themes/default/nav_main.php
+++ themes/default/nav_main.php
@@ -1,68 +1,37 @@
-  <div id="nav_main" class="row">
-  <div class="contain-to-grid sticky">
-  <nav class="top-bar large-12 medium-12 columns" data-topbar data-options="sticky_on: medium">
-    <ul class="title-area">
-     <!-- Title Area -->
-      <li class="name">
-        <h1>
-          <a href="#">
-            Domain.com
-          </a>
-        </h1>
-      </li>
-
-      <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
-    </ul>
-
+<div class="row">
+<div class="large-12 columns">
+<nav class="top-bar" data-topbar>
+<ul class="title-area">
+<li class="name">
+<h1><a href="#">My Site</a></h1>
+</li>
+<li class="toggle-topbar menu-icon"><a href="">Menu</a></li>
+</ul>
 <section class="top-bar-section">
-      <!-- Right Nav Section -->
-      <ul class="left">
-           <li class="has-form"> <a href="#" class="button">Home</a> </li>
-
-      </ul>
-      <ul class="right">
-
-        <li class="divider"></li>
-        <li class="has-dropdown not-click">
-          <a href="#">Menu</a>
-          <ul class="dropdown"><li class="title back js-generated"><h5><a href="javascript:void(0)">Back</a></h5></li>
-            <li><label>Section Name</label></li>
-            <li class="has-dropdown not-click">
-              <a href="#" class="">Has Dropdown, Level 1</a>
-              <ul class="dropdown"><li class="title back js-generated"><h5><a href="javascript:void(0)">Back</a></h5></li>
-                <li><a href="#">Dropdown Options</a></li>
-                <li><a href="#">Dropdown Options</a></li>
-                <li><a href="#">Level 2</a></li>
-                <li><a href="#">Subdropdown Option</a></li>
-                <li><a href="#">Subdropdown Option</a></li>
-                <li><a href="#">Subdropdown Option</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li class="divider"></li>
-            <li><label>Section Name</label></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li class="divider"></li>
-            <li><a href="#">See all →</a></li>
-          </ul>
-        </li>
-
-        <li class="divider"></li>
-        
-        <li class="has-dropdown not-click">
-          <a href="#">Admin</a>
-          <ul class="dropdown"><li class="title back js-generated"><h5><a href="javascript:void(0)">Back</a></h5></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li><a href="#">Dropdown Option</a></li>
-            <li class="divider"></li>
-            <li><a href="#">See all →</a></li>
-          </ul>
-        </li>
-      </ul>
-    </section></nav>
-    </div>
-    </div>
+ 
+<ul class="right show-for-large-up">
+<li class="active"><a href="#">Right Button Active</a></li>
+<li class="has-dropdown">
+<a href="#">Right Button Dropdown</a>
+<ul class="dropdown">
+<li><a href="#">First link in dropdown</a></li>
+</ul>
+</li>
+</ul>
+<ul class="right hide-for-large-up">
+<li class="active"><a href="#">Right Button</a></li>
+<li class="has-dropdown">
+<a href="#">Right Dropdown</a>
+<ul class="dropdown">
+<li><a href="#">First link in dropdown</a></li>
+</ul>
+</li>
+</ul>
+ 
+<ul class="left show-for-large-up">
+<li><a href="#">Left Button</a></li>
+</ul>
+</section>
+</nav>
+</div>
+</div>
