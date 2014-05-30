a694e4541abcbdf5ec03a3e80d6f2c3019bbc3ba
Dommert <DommertEnterprises@Gmail.com>
2014-05-29 21:50:09 -0700

    update
    more updates

---
+++ object/index.php
@@ -0,0 +1,36 @@
+<?PHP
+// Dommert Enterprises Inc
+// (c) 2014
+
+//----------------------------------
+// beta templates
+
+// Demo Template One column
+
+// ------- Load Globals & Page Info
+$page['title'] = "DEMO Page";
+$page['parent'] = "1";
+$theme = "default"; //Required Theme (folder)
+$config = $_SERVER['DOCUMENT_ROOT'].'/globals.php';
+  //$config = __DIR__ . '/globals.php';
+  REQUIRE_ONCE($config); // Load Configs & Libraries
+// Check Login Routine
+//------------------------------
+
+// -------- Content Section
+$head = $themes.'/head.php'; 
+$header = $themes.'/header.php';
+$nav = $themes.'/nav_main.php';
+$middle = $content.'/object/index.middle.php';
+$footer = $themes.'/footer.php';
+//--------------------------------
+
+// ** Load the Template ** 
+include $themes.'/main.php'; // Main Template
+
+?>
+
+
+
+
+
